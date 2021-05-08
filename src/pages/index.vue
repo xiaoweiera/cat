3a4564ff78fa96cd8885ad7508f9875c8c4e1074
yuanxuwei <yuanxuwei@jinse.com>
2021-05-08 10:57:03 +0800

    路由2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,6 @@ import { onMounted } from 'vue'
 onMounted(() => {
   const router = useRouter()
   const loc = window.location
-  console.log('index',loc,loc.host.startsWith('apy'),loc.pathname )
   // 二级域名 growthpad.xxx.com/ 跳转到 growthpad.xxx.com/growthpad
   // console.log(loc.host.startsWith('growthpad'), loc.host, loc.pathname)
   if (loc.host.startsWith('growthpad') && loc.pathname === '/') {
