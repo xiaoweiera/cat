c1068b998351742ebffbeb068c87c089a0cc73a1
yuanxuwei <yuanxuwei@jinse.com>
2021-05-18 19:05:09 +0800

    down下载被中英文切换挡住

--- src/components/apy/header.vue
+++ src/components/apy/header.vue
@@ -105,12 +105,12 @@ const showDialog = () => show.value = !show.value
         src="https://res.ikingdata.com/nav/topicLogo.png"
         alt=""
     /></a>
-    <div @click="toggleLocales()" class="flex items-center hand">
-      <div class="mr-1 text-global-default opacity-85 ml-kd32px i8n-font-inter"> 中文</div>
-      <img class="w-6 h-6" src="https://res.ikingdata.com/nav/growLang.png" alt="">
+    <div  class="flex items-center hand">
+<!--      <div class="mr-1 text-global-default opacity-85 ml-kd32px i8n-font-inter"> 中文</div>-->
+<!--      <img class="w-6 h-6" src="https://res.ikingdata.com/nav/growLang.png" alt="">-->
       <a href="https://www.ikingdata.com/download" target="_blank"
          class="text-global-default opacity-85 ml-3 i8n-font-inter"
-      >下载</a>
+      >APP下载</a>
     </div>
   </div>
 </template>
