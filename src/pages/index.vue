055321dcb4530e77124382dd3baee2dfd70fe519
svonme <svon.me@gmail.com>
2021-05-20 12:57:40 +0800

    feat: index 替换为 apy内容

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,247 @@
 <script setup lang="ts">
+import {onMounted, onUnmounted, reactive, ref, watch} from 'vue'
 import { useRouter } from 'vue-router'
-import { onMounted } from 'vue'
-onMounted(() => {
-  const router = useRouter()
-  const loc = window.location
-  // 二级域名 growthpad.xxx.com/ 跳转到 growthpad.xxx.com/growthpad
-  // console.log(loc.host.startsWith('growthpad'), loc.host, loc.pathname)
-  if (loc.host.startsWith('growthpad') && loc.pathname === '/') {
-    router.push('/growthpad')
-  } else if (loc.host.startsWith('apy') && loc.pathname === '/') {
-    router.push('/apy')
+import {requestTables, defaultChains, requestChart} from '~/logic/apy'
+// @ts-ignore
+import {chainConfig, tableConfig, anchorConfig, chartsConfig} from '~/logic/apy/config'
+import {wxShare} from '~/lib/wxShare'
+const {chains} = defaultChains(chainConfig)
+const chainParam = ref('')
+// @ts-ignore
+const { tables, requestData: fetchTableData } = requestTables()
+// @ts-ignore
+const { charts, requestChartData: fetchChartData } = requestChart()
+const selectedAnchor = ref('机枪池APY')
+// const chainQuery=ref('')
+// const router=useRouter()
+// console.log(router.params)
+// @ts-ignore
+
+// 计算元素距离 body 的位置
+const offsetY = function(dom: HTMLElement, number = 0): number {
+  const body = document.body || document.querySelector('body')
+  if (dom) {
+    const value = number + dom.offsetTop
+    const parent = dom.offsetParent
+    if (parent === body) {
+      return value
+    }
+    return offsetY(parent, value)
   }
+  return number
+}
+
+const clickAnchor = (name: string, key?: string) => {
+  if (name === '回到顶部') {
+    // @ts-ignore
+    document.scrollingElement.scrollTop = 0
+  }
+  selectedAnchor.value = name
+  if (key) {
+    const top = offsetY(document.querySelector(`#content-${key}`))
+    document.scrollingElement.scrollTop = top
+  }
+}
+const fetchTableByChain = (chain: String) => {
+  tableConfig.map((i, idx) => {
+    return fetchTableData(idx, i.name, i.title, i.options, {
+      chain,
+      category: i.name,
+    })
+  })
+}
+const fetchChartByChain = (chain: String) => {
+  tableConfig.map((chartItem, tableIndex) => {
+    chartItem.charts.map((chart: any, chartIndex: number) => {
+      return fetchChartData(tableIndex, chartIndex, chartItem.name, chart.title, chart.requestData, chart.chartData, chart.xyData, {
+        chain,
+        category: chartItem.name,
+        ...chart.param,
+      })
+    })
+  })
+}
+
+const timer = ref(60)
+let timerInterval: any = null
+const isFirstShow = ref(true)
+const newTable = ref([])
+const intervalFetchTableByChain = (chainId: string, timeout = 60) => {
+  chainParam.value = chainId
+  fetchTableByChain(chainId)
+  fetchChartByChain(chainId)
+  timerInterval = setInterval(() => {
+    if (timer.value !== 0) {
+      timer.value -= 1
+      return
+    }
+    isFirstShow.value = false
+    timer.value = timeout
+    isFirstShow.value = false
+    fetchTableByChain(chainId)
+    fetchChartByChain(chainId)
+  }, 1000)
+}
+watch(() => chains.data, (newVal) => {
+  if (timerInterval) {
+    clearInterval(timerInterval)
+    isFirstShow.value = true
+    timer.value = 60
+    timerInterval = null
+  }
+  newVal.forEach((i: any) => {
+    if (i.select) {
+      chainParam.value = i.key
+      intervalFetchTableByChain(i.key)
+    }
+  })
 })
+const selectedMobileAnchor = reactive({ name: '机枪池APY' })
+onMounted(() => {
+  wxShare('DeFi挖矿收益APY大全', '全网最全的挖矿收益APY大全，数百家项目数据多维度对比。')
+  // intervalFetchTableByChain('heco')
+})
+onUnmounted(() => clearInterval(timerInterval))
 </script>
+<template>
+  <div class="flex-col w-full max-w-360  md:mb-25">
+    <!-- 头部描述信息-->
+    <div class="px-4 md:px-30">
+      <!-- 大屏时隐藏 -->
+      <div class="mdhidden mt-6">
+        <ApyBanner></ApyBanner>
+      </div>
+      <div class="flex justify-center items-center mt-4 md:mt-15 ">
+        <div style="font-weight: bold" class="flex justify-center mr-2 md:mr-3 text-kd24px100  text-global-highTitle md:text-kd36px36px">
+          DeFi挖矿收益APY大全
+        </div>
+        <a href="http://ikingdata.mikecrm.com/ijyjMFO?utm_source=https://apy.kingdata.com" target="_blank"
+           class="goForm text-kd12px20px font-normal">申请收录</a>
+      </div>
+      <div class="mt-4 text-global-default opacity-65 font-normal ">
+        <div class="text-kd14px22px md:text-center">本站收集整理了多条公链各借贷平台和机枪池的数据,根据类型将其分类方便您的查看。</div>
+        <div style="color:#E9592D;" class="text-kd12px18px md:text-center mt-1">
+          风险提示：本站数据来源于各平台的公开数据，本站并未对收录内容做安全审计，内容不构成投资建议，请注意风险。
+        </div>
+      </div>
+      <div class="text-center flex justify-center md:mb-5">
+        <!-- active 默认选中项 -->
+        <ApyChains :chains="chains" active="heco" />
+      </div>
+      <!-- 小屏时隐藏 -->
+      <div class="xshidden">
+        <ApyAds></ApyAds>
+      </div>
+    </div>
+    <!-- table表格-->
+    <div :class="index%2!==0 ? ' tableDefault':'tableDefault' "
+         v-for="(item,index) in tables" :key="index">
+      <!-- chain type 等于 hsc(hoo) 时，不展示 单币种机枪池 APY 对比 -->
+      <template v-if="chainParam === 'hsc' && index === 0 ">
+        <div></div>
+      </template>
+      <div v-else :id="`content-${item.slug}`">
+        <a class="mdhidden">
+          <ApyMobileTag :title="item.slug" :tableIndex="index" :selectedMobileAnchor="selectedMobileAnchor"/>
+        </a>
+        <ApyTable :chains="chainParam" :isFirstShow="isFirstShow" :timer="timer" :index="index"
+                  :project="item.project" :title="item.title"
+                  :tableData="item"/>
+        <div class="grid  md:gap-10 grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
+          <template v-for="(itemChart,i) in charts[index].chartAll" :key="`${index}-${i}`">
+            <ApyChart v-show="itemChart.option" :chainId="chainParam" :tableIndex="index" :chartIndex="i"
+                      :chartData="itemChart" :id="index+''+i"/>
+          </template>
+        </div>
+      </div>
+    </div>
+    <ApyFooter/>
+    <!--浮动tag-->
+    <div>
+      <div class="tagContainer  w-19 h-28 flex-col 2xl:right-10 fixed right-2 top-70">
+        <template v-for="(item, index) in anchorConfig" :key="index">
+          <a class="hand" @click="clickAnchor(item.name, item.key)"
+             :class="selectedAnchor===item.name? 'floatRightTag  selectRightTag hand':'floatRightTag  rightTag hand' "
+          >{{ item.name }}</a>
+        </template>
+      </div>
+      <img @click="clickAnchor('回到顶部')" class="mdhidden bottom-10 right-5 w-11 h-11 hand"
+           src="https://res.ikingdata.com/nav/apyBack.png">
+    </div>
+  </div>
+</template>
+<style scoped lang="postcss">
+.tableDefault {
+  @apply px-4 md:px-30 ;
+}
+
+@media screen and (max-width: 768px) {
+  .heightAuto {
+    height: calc(100vh - 357px);
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .heightAuto {
+    height: calc(100vh - 417px);
+  }
+}
+
+.floatRightTag {
+  @apply text-kd12px16px font-medium mb-4  pl-2;
+}
+
+.selectRightTag {
+
+  border-left: 2px solid #2B8DFE;
+  @apply relative right-0.3 block  text-global-primary;
+}
+
+.rightTag {
+  @apply text-global-default block opacity-65;
+}
+
+.goForm {
+  padding: 4px 12px;
+  color: rgba(254, 254, 254, 1);
+  /* color / 主色 */
+  background: #2B8DFE;
+  border-radius: 34px;
+}
+
+.cardBg {
+  background-image: url('https://res.ikingdata.com/nav/apyCardBg.png');
+  background-size: cover;
+}
+
+.selected {
+
+}
+
+.tagContainer {
+  border-left: 1px solid rgba(37, 62, 111, 0.1);
+}
+
+
+@media screen and (max-width: 880px) {
+  .tagContainer {
+    display: none;
+  }
+}
+
+.rightTag {
+  box-shadow: inset 1px 0px 0px rgba(37, 62, 111, 0.1);
+}
+
+.leftBorder {
+  border-left: 2px solid #2B8DFE;
+}
+</style>
+
+// @formatter:off
+<route lang="yaml">
+meta:
+ layout: home
+</route>
+// @formatter:off
+
