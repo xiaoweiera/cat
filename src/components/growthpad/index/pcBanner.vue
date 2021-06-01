d05062a25994813ca9f8dce03c65fda301e083f8
yuanxuwei <yuanxuwei@jinse.com>
2021-06-01 16:33:14 +0800

    fix index邀请链接规则更改

--- src/components/growthpad/index/pcBanner.vue
+++ src/components/growthpad/index/pcBanner.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import { company } from '~/logic/growthpad/index'
-import { userData } from '~/logic/user/login'
+import { userData, isLogin } from '~/logic/user/login'
 import { copyTxt } from '~/lib/tool'
 import I18n from '~/utils/i18n'
+// const link = (): string => {
+//   const { origin, pathname } = window.location
+//   let url = `${origin}${pathname}`
+//   const code = userData.my_invitation_code
+//   if (code) {
+//     url = `${url}?code=${code}`
+//   }
+//   return url
+// }
+// @ts-ignore
 const link = (): string => {
   const { origin, pathname } = window.location
   let url = `${origin}${pathname}`
-  const code = userData.my_invitation_code
+  const code = `${userData.my_invitation_code}-G0`
   if (code) {
     url = `${url}?code=${code}`
   }
-  return url
+  const $title = document.querySelector('title')
+  const value = $title.innerText
+  return I18n.template(I18n.growthpad.mdx.copy, { title: value, url })
 }
 </script>
 <template>
@@ -44,7 +56,7 @@ const link = (): string => {
         <div>
           <span>{{ I18n.growthpadShow.iInvite }}</span><span class="numText">{{ userData.growthpad_invited_count }}</span>
         </div>
-        <div class="flex items-end">
+        <div v-if="isLogin" class="flex items-end">
           <span class="">{{ I18n.growthpadShow.code }}</span>
           <span class="text-kd17px17px text-global-primary ml-1.5 numText">{{
             userData.my_invitation_code
@@ -57,27 +69,24 @@ const link = (): string => {
             ></IconFont>
           </Copy>
           <Copy :link="link">
-            <div
-              class="
-                visited
-                ml-6
-                flex
-                justify-center
-                items-center
-                text-kd16px24px
-                font-kdFang
-                hand
-              "
-            >
+            <div class="visitedButton">
               {{ I18n.growthpadShow.invite }}
             </div>
           </Copy>
         </div>
+        <div v-else v-login class="visitedButton">请登录</div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
+.visitedButton {
+  color: #ffffff;
+  background: linear-gradient(180deg, #7cb9ff 0%, #3090ff 100%);
+  border-radius: 42px;
+  padding: 4px 12px;
+  @apply visited ml-6 flex justify-center items-center text-kd16px24px font-kdFang cursor-pointer;
+}
 .numText {
   @apply text-kd24px24px text-global-primary font-kdExp ml-1.5;
 }
@@ -85,13 +94,6 @@ const link = (): string => {
   border: 1px solid rgba(43, 141, 254, 0.06);
   box-shadow: 0px 12px 72px -12px rgba(43, 141, 254, 0.13);
   border-radius: 24px;
-}
-.visited {
-  color: #ffffff;
-
-  background: linear-gradient(180deg, #7cb9ff 0%, #3090ff 100%);
-  border-radius: 42px;
-  padding: 4px 12px;
 }
 .mobileImg {
   @apply flex items-center   justify-center  w-full pt-4 px-4;
