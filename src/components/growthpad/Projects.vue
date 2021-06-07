7c28a2b23cd444eaff49d9c551bd8742ea32472e
svonme <svon.me@gmail.com>
2021-06-07 16:33:09 +0800

    fix:#KD-152 growthpad 首页样式调整

--- src/components/growthpad/Projects.vue
+++ src/components/growthpad/Projects.vue
@@ -25,50 +25,55 @@ onMounted(() => {
 </script>
 <template>
   <div class="md:block hidden beginTxt">{{ props.title }}</div>
-  <div class="md:mt-6 md:flex md:justify-between md:flex-wrap">
-    <div class="w-full md:max-w-97 mt-4 min-w-96">
-      <GrowthpadProject
-        :value="
-          mdxStatus.value === 'wait'
-            ? mdxInfo.dashboard.end
-            : mdxInfo.dashboard.begin
-        "
-        :status="mdxStatus"
-        :project="mdxInfo"
-      />
-    </div>
-    <div class="w-full md:max-w-97 mt-4 min-w-96">
-      <GrowthpadProject
-        :value="
-          coinwindStatus.value === 'wait'
-            ? coinwindInfo.dashboard.end
-            : coinwindInfo.dashboard.begin
-        "
-        :status="coinwindStatus"
-        :project="coinwindInfo"
-      />
-    </div>
-    <div class="w-full md:max-w-97 mt-4 min-w-96">
-      <GrowthpadProject
-        :value="
-          channelsStatus.value === 'wait'
-            ? channelsInfo.dashboard.end
-            : channelsInfo.dashboard.begin
-        "
-        :status="channelsStatus"
-        :project="channelsInfo"
-      />
-    </div>
-    <div v-if="!projectName" class="w-full md:max-w-97 mt-4 min-w-96">
-      <GrowthpadProject
-        :value="
-          growthpadStatus.value === 'wait'
-            ? growthInfo.dashboard.end
-            : growthInfo.dashboard.begin
-        "
-        :status="growthpadStatus"
-        :project="growthInfo"
-      />
+  <div>
+    <div class="md:-m-3 md:flex md:flex-wrap">
+      <div class="mt-6 w-full lg:w-1/3 md:w-1/2 md:px-3 min-w-96">
+        <GrowthpadProject
+          :value="
+            mdxStatus.value === 'wait'
+              ? mdxInfo.dashboard.end
+              : mdxInfo.dashboard.begin
+          "
+          :status="mdxStatus"
+          :project="mdxInfo"
+        />
+      </div>
+      <div class="mt-6 w-full lg:w-1/3 md:w-1/2 md:px-3 min-w-96">
+        <GrowthpadProject
+          :value="
+            coinwindStatus.value === 'wait'
+              ? coinwindInfo.dashboard.end
+              : coinwindInfo.dashboard.begin
+          "
+          :status="coinwindStatus"
+          :project="coinwindInfo"
+        />
+      </div>
+      <div class="mt-6 w-full lg:w-1/3 md:w-1/2 md:px-3 min-w-96">
+        <GrowthpadProject
+          :value="
+            channelsStatus.value === 'wait'
+              ? channelsInfo.dashboard.end
+              : channelsInfo.dashboard.begin
+          "
+          :status="channelsStatus"
+          :project="channelsInfo"
+        />
+      </div>
+      <div
+        v-if="!projectName"
+        class="mt-6 w-full lg:w-1/3 md:w-1/2 md:px-3 min-w-96"
+      >
+        <GrowthpadProject
+          :value="
+            growthpadStatus.value === 'wait'
+              ? growthInfo.dashboard.end
+              : growthInfo.dashboard.begin
+          "
+          :status="growthpadStatus"
+          :project="growthInfo"
+        />
+      </div>
     </div>
   </div>
 </template>
