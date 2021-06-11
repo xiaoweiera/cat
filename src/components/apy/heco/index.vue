5a5482cc411cf88be3e4c782ef29eadf62f0e4da
svonme <svon.me@gmail.com>
2021-06-11 19:35:00 +0800

    feat:#KD-228 heco 数据处理

--- src/components/apy/heco/index.vue
+++ src/components/apy/heco/index.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { getHecoDetail } from '~/api/apy'
-import { header } from './props'
+// @ts-ignore
+import { header, transform } from '~/logic/apy/heco'
 const datalist = ref<anyp[]>([])
 
 onMounted(async () => {
   const result = await getHecoDetail()
   const { list = [], total = 0 } = result
-  datalist.value = list
+  datalist.value = transform(list)
 })
 </script>
 
 <template>
   <div class="flex-1" :class="{ max: false }">
     <el-table :data="datalist" stripe class="w-full">
+      <el-table-column
+        type="index"
+        width="50"
+        header-align="center"
+      ></el-table-column>
       <template v-for="(item, index) in header" :key="index">
-        <el-table-column :prop="item.key" :label="item.label"></el-table-column>
+        <!-- 节点名称 -->
+        <el-table-column
+          v-if="item.key === 'node_name'"
+          :prop="item.key"
+          :label="item.label"
+        >
+          <template #default="scope">
+            <div class="flex items-center">
+              <IconFont
+                v-if="scope.row.icon_url"
+                :type="scope.row.icon_url"
+                class="mr-2"
+                size="2xl"
+              ></IconFont>
+              <p>{{ scope.row[item.key] }}</p>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-else
+          :prop="item.key"
+          :label="item.label"
+        ></el-table-column>
       </template>
     </el-table>
   </div>
---
+++ src/logic/apy/heco.ts
@@ -0,0 +1,46 @@
+/**
+ * @file heco
+ * @author svon.me@gmail.com
+ */
+
+import { formatCash, toNumber } from '~/utils/index'
+
+export interface TD {
+  label: string
+  key: string
+}
+
+export const header: TD[] = [
+  { label: '节点名称', key: 'node_name' },
+  { label: '所在地区', key: 'country_name_cn' },
+  { label: '得票数', key: 'vote_count' },
+  { label: '得票占比', key: 'vote_percent' },
+  { label: '收益分成比例', key: 'rate' },
+  { label: 'APY', key: 'apy' },
+]
+
+export const transform = function (list: any[]) {
+  const array: any[] = []
+  for (let i = 0, size = list.length; i < size; i++) {
+    const data = Object.assign({}, list[i])
+    for (let k = 0, len = header.length; k < len; k++) {
+      const item: TD = header[k]
+      // 得票数
+      if (item.key === 'vote_count') {
+        const number = toNumber(data[item.key])
+        data[item.key] = formatCash(number)
+      } else if (item.key === 'vote_percent') {
+        const number = toNumber(data[item.key])
+        data[item.key] = `${number}%`
+      } else if (item.key === 'rate') {
+        const number = toNumber(data[item.key])
+        data[item.key] = `${number}%`
+      } else if (item.key === 'apy') {
+        const number = toNumber(data[item.key])
+        data[item.key] = `${number}%`
+      }
+    }
+    array.push(data)
+  }
+  return array
+}
