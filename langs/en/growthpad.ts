486aa57bc397a4e79c7c0032d30a021762511539
svonme <svon.me@gmail.com>
2021-06-16 14:37:17 +0800

    feat:#KD-248 修改 growthpad 文案，去掉前xx用户

--- langs/en/growthpad.ts
+++ langs/en/growthpad.ts
@@ -15,8 +15,10 @@ export default {
     perPerson: 'The Max Reward Per Person',
     total: 'Reward {count} {token}',
     maxTotal: '{count} {token} at maximum',
-    finished: 'Finish all tasks: {reward}（TOP {count} users）',
-    anytask: 'Finish one of tasks: {reward}（TOP {count} users）',
+    // finished: 'Finish all tasks: {reward}（TOP {count} users）',
+    // anytask: 'Finish one of tasks: {reward}（TOP {count} users）',
+    finished: 'Finish all tasks: {reward}',
+    anytask: 'Finish one of tasks: {reward}',
   },
   register: 'Information Registration',
   submit: 'Register',
@@ -117,8 +119,10 @@ export default {
     twitter: 'This activity'
   },
   front: {
-    '500': '{value}（TOP 500 users）',
-    '2000': '{value}（TOP 2,000 users）'
+    // '500': '{value}（TOP 500 users）',
+    // '2000': '{value}（TOP 2,000 users）',
+    '500': '{value}',
+    '2000': '{value}'
   },
 
   wechat: {
@@ -266,7 +270,6 @@ CoinWind now supports the pledge of HECO and BSC mainchain ecologies, and will s
     },
     task2: {
       title: 'TVL in Pancake Bunny is more than 10K USDT; Join CoinWind telegram; Follow CoinWind Twitter and retweet the twitter about the activity.',
-      description: 'Finished one of tasks: <i>reward 1MDX</i> (top 500 users)',
       condition1: 'Qualification: TVL in PanCake Bunny is more than 10K USDT.',
     },
     task3: {
