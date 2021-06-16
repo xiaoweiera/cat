8a34c1012aae9fd9418657475deb8e11ef921e05
svonme <svon.me@gmail.com>
2021-06-16 17:24:20 +0800

    feat:#KD-246 oss 前端文件直传

--- src/api/res.ts
+++ src/api/res.ts
@@ -2,10 +2,11 @@
  * @file 资源文件上传
  * @author svon.me@gmail.com
  */
-import I18n from '~/utils/i18n/index'
 import { last } from 'ramda'
 import { res } from './pathname'
 import request from '~/lib/service'
+import I18n from '~/utils/i18n/index'
+import { uuid } from '~/utils/index'
 import { LoginStatus } from '~/logic/user/check'
 import safeGet from '@fengqiaogang/safe-get'
 
@@ -22,29 +23,40 @@ export const getPolicy = LoginStatus(res.policy, async () => {
   }
 })
 
-const getFileName = function(file: File): string {
-  const name: string = file.name
-  const arr: string[] = name.split('/')
-  return last<string>(arr) as any
-}
-// @ts-ignore
 const getSuffix = function(src: string): string {
   const indexof = src.lastIndexOf('.')
   if (indexof !== -1) {
-    return src.substring(indexof)
+    return src.substring(indexof + 1)
   }
   return '';
 }
+
+const getFileName = function(file: File): string {
+  const name: string = file.name
+  const arr: string[] = name.split('/')
+  const value = last<string>(arr) as any
+  const suffix = getSuffix(value)
+  const key = uuid()
+  return `${key}.${suffix}`
+}
+
 
 // 图片上传
 // @ts-ignore
 export const uploadImage = LoginStatus(res.image, async(file: File) => {
   const query = new FormData()
   const option: any = await getPolicy()
-  const key = '${filename}'
+
+  const name = getFileName(file)
+  const url = `${option.domain}/${option.dir}${name}`
+  console.log(url)
+
+  // const key = '${filename}' // 保留原文件名
+  const key = `${name}` // 使用随机名称
+
   query.append('OSSAccessKeyId', option.accessid)
   query.append('callback', option.callback)
-  query.append('name', getFileName(file))
+  query.append('name', name)
   query.append('success_action_status', '200')
   query.append('key', `${option.dir}${key}`)
   query.append('policy', option.policy)
@@ -53,11 +65,10 @@ export const uploadImage = LoginStatus(res.image, async(file: File) => {
 
   try {
     const result = await request.post(option.host, query)
-    console.log(result)
-    // const data = result?.data || {}
-    // if (data?.code === 0) {
-    //   return data.url
-    // }
+    const status = safeGet<string>(result, 'data.Status')
+    if (status === 'ok') {
+      return url
+    }
   } catch (e) {
     // todo
   }
