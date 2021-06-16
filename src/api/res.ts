b9aec331f72395a572f64272b37960e0d5c753ed
svonme <svon.me@gmail.com>
2021-06-16 18:23:50 +0800

    perf:#KD-246 优化文件名生成逻辑

--- src/api/res.ts
+++ src/api/res.ts
@@ -24,9 +24,11 @@ export const getPolicy = LoginStatus(res.policy, async () => {
 })
 
 const getSuffix = function(src: string): string {
-  const indexof = src.lastIndexOf('.')
-  if (indexof !== -1) {
-    return src.substring(indexof + 1)
+  const indexOf: number = src.lastIndexOf('.')
+  if (indexOf !== -1) {
+    const value = src.substring(indexOf + 1)
+    // 去掉开头为 . 的字符
+    return value.replace(/^\.+/, '')
   }
   return '';
 }
@@ -40,20 +42,25 @@ const getFileName = function(file: File): string {
   return `${key}.${suffix}`
 }
 
+const error = {
+  message: I18n.common.api.wrong
+}
 
 // 图片上传
 // @ts-ignore
 export const uploadImage = LoginStatus(res.image, async(file: File) => {
-  const query = new FormData()
+  // 获取签名
   const option: any = await getPolicy()
-
+  // 签名为空时
+  if (!option.policy) {
+    // 提示错误信息
+    return Promise.reject(error)
+  }
+  const query = new FormData()
+  // 生成文件名称
   const name = getFileName(file)
-  const url = `${option.domain}/${option.dir}${name}`
-  console.log(url)
-
   // const key = '${filename}' // 保留原文件名
   const key = `${name}` // 使用随机名称
-
   query.append('OSSAccessKeyId', option.accessid)
   query.append('callback', option.callback)
   query.append('name', name)
@@ -61,18 +68,19 @@ export const uploadImage = LoginStatus(res.image, async(file: File) => {
   query.append('key', `${option.dir}${key}`)
   query.append('policy', option.policy)
   query.append('signature', option.signature)
+  // file 文件必须放在最后
   query.append('file', file)
-
   try {
     const result = await request.post(option.host, query)
     const status = safeGet<string>(result, 'data.Status')
     if (status === 'ok') {
+      // 拼接资源链接地址
+      const url = `${option.domain}/${option.dir}${name}`
       return url
     }
   } catch (e) {
     // todo
   }
-  return Promise.reject({
-    message: I18n.common.api.wrong
-  })
+  // 提示错误信息
+  return Promise.reject(error)
 })
