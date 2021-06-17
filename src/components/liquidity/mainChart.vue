161e7be442a2e2e827ff3f89f6c7ce9c66f80610
yuanxuwei <yuanxuwei@jinse.com>
2021-06-17 21:36:59 +0800

    jj

--- src/components/liquidity/mainChart.vue
+++ src/components/liquidity/mainChart.vue
@@ -1,28 +1,34 @@
 <script setup lang="ts">
-import { onMounted,ref } from 'vue'
+import { onMounted,ref,watch } from 'vue'
 import { echartData } from '/mock/liquidity'
+import { pairStore, sidePair } from '~/store/liquidity/state'
 import {getChartsFun} from '~/logic/liquidity/getChartData'
 const chartsData=ref()
-onMounted(async()=>{
+watch(()=>sidePair.pair_id,(n,o)=>{
+  getChartsData(n)
+})
+const getChartsData=async (pair_id:string)=>{
   const param={
     platId:1,
-    symbol_id: '0xe36ffd17b2661eb57144ceaef942d95295e637f0',
+    symbol_id: pair_id?pair_id:'0xe36ffd17b2661eb57144ceaef942d95295e637f0',
     from_ts:1581765635,
     to_ts:1617235200,
     interval:'1H'
   }
   const result= await getChartsFun(param)
-  console.log(result)
-    chartsData.value=result.data
-
+  chartsData.value=result.data
+}
+onMounted(()=>{
+  getChartsData()
 })
 </script>
 <template>
-  <div class="flex flex-1 h-full flex-col bg-global-body px-5 pt-3 chartContainer">
+  <div v-if="chartsData && chartsData?.length>0" class="flex flex-1 h-full flex-col bg-global-body px-5 pt-3 chartContainer">
     <template v-for="item in chartsData">
       <LiquidityChartContainer :chart-data="item" />
     </template>
   </div>
+  <div>无数据</div>
 </template>
 <style scoped lang="postcss">
 .chartContainer {
