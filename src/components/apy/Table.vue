121b0a93190804a4c8232a062af18a04c3a3fc60
yuanxuwei <yuanxuwei@jinse.com>
2021-06-17 17:50:46 +0800

    fix 拆分

--- src/components/apy/Table.vue
+++ src/components/apy/Table.vue
@@ -86,11 +86,9 @@ const getPlatInfo = (name: string) => {
 // 单元格背景色
 // @ts-ignore
 const addClass = ({ row, columnIndex }) => {
-  if (
-    columnIndex > 0 &&
-    row.data[columnIndex - 1] &&
-    row.data[columnIndex - 1]?.single_high_light
-  ) {
+  const single=options.value.data.find(item=>item.key==='single_and_mine_award')
+  const apyType=single.status?'single_high_light':'compound_high_light'
+  if (columnIndex > 0 && row.data[columnIndex - 1] && row.data[columnIndex - 1]?.[apyType]) {
     return 'background:rgba(9, 217, 142, 0.2); padding-top:0px; padding-bottom: 0;'
   }
   return 'background: #F6FAFD;'
@@ -130,38 +128,15 @@ const getValue = (data: any) => {
 </script>
 <template>
   <div v-if="props.tableData.rows" class="tableHeaderTop">
-    <ApyTableFilters
-      :timer="timer"
-      :project="tableData.slug"
-      :options="options"
-      :title="title"
-    />
+    <ApyTableFilters :timer="timer" :project="tableData.slug" :options="options" :title="title"/>
     <div class="flex flex-col relative minWidth">
-      <img
-        v-if="tableData.loading && isFirstShow"
-        class="loading"
-        src="/assets/loading.gif"
-        alt=""
-      />
+      <img v-if="tableData.loading && isFirstShow" class="loading" src="/assets/loading.gif" alt=""/>
       <div class="xshidden">
         <!-- pc -->
-        <el-table
-          v-if="!tableData.loading || (tableData.loading && !isFirstShow)"
-          :data="renderCells"
-          :header-cell-style="headerCellStyle"
-          :cell-style="addClass"
-          style="width: 100%"
-        >
+        <el-table v-if="!tableData.loading || (tableData.loading && !isFirstShow)" :data="renderCells" :header-cell-style="headerCellStyle" :cell-style="addClass" style="width: 100%">
           <el-table-column fixed width="140">
             <template #header="scope">
-              <div
-                class="
-                  text-kd12px16px text-global-default
-                  opacity-65
-                  mb-2.5
-                  ml-3
-                "
-              >
+              <div class="text-kd12px16px text-global-default opacity-65 mb-2.5 ml-3">
                 <span>{{ I18n.apy.tableHeader.type }}</span>
               </div>
               <div class="text-kd12px16px text-global-default opacity-65 ml-3">
@@ -170,39 +145,20 @@ const getValue = (data: any) => {
             </template>
             <template #default="scope">
               <div class="justify-center flex flex-col">
-                <a
-                  class="flex flex-col hand pl-3"
-                  :href="scope.row.url + '?utm_source=https://apy.kingdata.com'"
-                  target="_blank"
-                >
+                <a class="flex flex-col hand pl-3" :href="scope.row.url + '?utm_source=https://apy.kingdata.com'" target="_blank">
                   <div class="flex text-left items-center">
                     <!-- 隐藏 icon-->
                     <!-- <img class="w-8 h-8 mr-2" :src="scope.row.icon" alt="" />-->
-                    <div
-                      class="
-                        font-kdExp
-                        text-kd14px18px text-global-highTitle
-                        font-normal
-                      "
-                    >
+                    <div class="font-kdExp text-kd14px18px text-global-highTitle font-normal">
                       <span>{{ scope.row.project_name }}</span>
                     </div>
                     <!-- 展示项目是否为新项目 -->
                     <ApyBadge class="flex ml-1" :time="scope.row.online_time"></ApyBadge>
                   </div>
                   <div class="flex mt-2 items-center">
-                    <!--                    <div class="tableItemType">-->
-                    <!--                      <span v-if="props.index == 0">{{ I18n.apy.vaults }}</span>-->
-                    <!--                      <span v-else>{{ I18n.apy.lendPlat }}</span>-->
-                    <!--                    </div>-->
-                    <div
-                      v-if="props.chains === 'all'"
-                      class="tableItemTypePlat"
-                      :style="{
+                    <div v-if="props.chains === 'all'" class="tableItemTypePlat" :style="{
                         background: getPlatInfo(scope.row.chain).bgcolor,
-                        color: getPlatInfo(scope.row.chain).color,
-                      }"
-                    >
+                        color: getPlatInfo(scope.row.chain).color}">
                       <span>{{ getPlatInfo(scope.row.chain).name }}</span>
                     </div>
                   </div>
@@ -210,42 +166,15 @@ const getValue = (data: any) => {
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            v-for="(item, i) in headers"
-            :key="`${index}-${i}-${item.token_name}`"
-            min-width="132"
-          >
+          <el-table-column v-for="(item, i) in headers" :key="`${index}-${i}-${item.token_name}`" min-width="132">
             <template #header="scope">
-              <ApyHeaderColumn
-                :order-by-apy="orderByApy"
-                :select-header-index="selectHeaderIndex"
-                :header-index="i"
-                :header-data="item"
-              />
+              <ApyHeaderColumn :order-by-apy="orderByApy" :select-header-index="selectHeaderIndex" :header-index="i" :header-data="item"/>
             </template>
             <template #default="scope">
-              <el-popover
-                class="mt-10 py-10"
-                :offset="-6"
-                :width="300"
-                :show-arrow="isTipArrow"
-                :disabled="
-                  !isShowTip ||
-                  !isNullFun(scope.row?.data[scope.column.no - 1]?.data)
-                "
-                effect="light"
-                trigger="hover"
-                placement="bottom"
-              >
+              <el-popover class="mt-10 py-10" :offset="-6" :width="300" :show-arrow="isTipArrow" :disabled="!isShowTip || !isNullFun(scope.row?.data[scope.column.no - 1]?.data)" effect="light" trigger="hover" placement="bottom">
                 <template #default>
-                  <template
-                    v-for="(item, i) in scope.row?.data[i]?.data"
-                    :key="i"
-                  >
-                    <div
-                      v-if="getValue(item, i) !== '-'"
-                      class="flex mb-0.5 items-center flex-wrap TipTxt"
-                    >
+                  <template v-for="(item, i) in scope.row?.data[i]?.data" :key="i">
+                    <div v-if="getValue(item, i) !== '-'" class="flex mb-0.5 items-center flex-wrap TipTxt">
                       <span class="mr-1">{{ item.name }}</span>
                       <div>
                         <span>{{ getValue(item, i) }}</span>
@@ -255,11 +184,7 @@ const getValue = (data: any) => {
                 </template>
                 <template #reference>
                   <div>
-                    <ApyTableItem
-                      :scope-data="scope"
-                      :index="index"
-                      :item-data="scope.row.data[i]?.data"
-                    />
+                    <ApyTableItem :scope-data="scope" :index="index" :item-data="scope.row.data[i]?.data"/>
                   </div>
                 </template>
               </el-popover>
@@ -269,18 +194,10 @@ const getValue = (data: any) => {
       </div>
       <!--    手机-->
       <div class="mdhidden">
-        <el-table
-          v-if="!tableData.loading || (tableData.loading && !isFirstShow)"
-          :data="renderCells"
-          :header-cell-style="headerCellStyle"
-          :cell-style="addClass"
-          style="width: 100%"
-        >
+        <el-table v-if="!tableData.loading || (tableData.loading && !isFirstShow)" :data="renderCells" :header-cell-style="headerCellStyle"  :cell-style="addClass" style="width: 100%">
           <el-table-column fixed width="103">
             <template #header="scope">
-              <div
-                class="text-kd12px16px text-global-default opacity-65 mb-2.5"
-              >
+              <div class="text-kd12px16px text-global-default opacity-65 mb-2.5">
                 <span>{{ I18n.apy.tableHeader.type }}</span>
               </div>
               <div class="text-kd12px16px text-global-default opacity-65">
@@ -289,31 +206,10 @@ const getValue = (data: any) => {
             </template>
             <template #default="scope">
               <div class="justify-center flex flex-col">
-                <a
-                  class="
-                    flex flex-col flex-wrap
-                    justify-center
-                    items-center
-                    hand
-                  "
-                  :href="scope.row.url + '?utm_source=https://apy.kingdata.com'"
-                  target="_blank"
-                >
+                <a class="flex flex-col flex-wrap justify-center items-center hand" :href="scope.row.url + '?utm_source=https://apy.kingdata.com'"  target="_blank">
                   <!-- 隐藏 icon -->
-                  <!-- <img class="md:w-8 mx-auto my-0 md:h-8 w-6 h-6 md:mr-1.5" :src="scope.row.icon" alt=""/> -->
                   <div class="flex flex-col">
-                    <div
-                      class="
-                        font-kdExp
-                        ml-0.5
-                        text-center
-                        mb-1
-                        text-kd12px18px
-                        md:text-kd14px18px
-                        text-global-highTitle
-                        font-normal
-                      "
-                    >
+                    <div class="font-kdExp ml-0.5 text-center mb-1 text-kd12px18px md:text-kd14px18px text-global-highTitle font-normal">
                       <span>{{ scope.row.project_name }}</span>
                     </div>
                     <div class="text-center">
@@ -330,19 +226,12 @@ const getValue = (data: any) => {
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            v-for="(item, i) in headers"
-            :key="`${index}-${i}-${item.token_name}`"
-            min-width="140"
-          >
+          <el-table-column v-for="(item, i) in headers" :key="`${index}-${i}-${item.token_name}`" min-width="140">
             <template #header="scope">
               <ApyHeaderColumn :order-by-apy="orderByApy" :header-data="item" />
             </template>
             <template #default="scope">
-              <ApyTableItem
-                :index="index"
-                :item-data="scope.row.data[i]?.data"
-              />
+              <ApyTableItem :index="index" :item-data="scope.row.data[i]?.data"/>
             </template>
           </el-table-column>
         </el-table>
