4b8238166d952b26a7ddb4e3f3bc07fc6855dc22
yuanxuwei <yuanxuwei@jinse.com>
2021-06-17 21:47:13 +0800

    fix liquidity demo

--- src/components/liquidity/mainChart.vue
+++ src/components/liquidity/mainChart.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-import { onMounted,ref,watch } from 'vue'
+import { onMounted,ref,watch,reactive} from 'vue'
 import { echartData } from '/mock/liquidity'
+import {useRoute, useRouter} from 'vue-router'
 import { pairStore, sidePair } from '~/store/liquidity/state'
 import {getChartsFun} from '~/logic/liquidity/getChartData'
+const route = useRoute()
+const router = useRouter()
+const routeQuery = reactive(route.query)
 const chartsData=ref()
 watch(()=>sidePair.pair_id,(n,o)=>{
   getChartsData(n)
@@ -10,11 +14,12 @@ watch(()=>sidePair.pair_id,(n,o)=>{
 const getChartsData=async (pair_id:string)=>{
   const param={
     platId:1,
-    symbol_id: pair_id?pair_id:'0xe36ffd17b2661eb57144ceaef942d95295e637f0',
+    symbol_id: routeQuery.pair?routeQuery.pair:'0xe36ffd17b2661eb57144ceaef942d95295e637f0',
     from_ts:1581765635,
     to_ts:1617235200,
     interval:'1H'
   }
+  console.log(routeQuery.pair)
   const result= await getChartsFun(param)
   chartsData.value=result.data
 }
