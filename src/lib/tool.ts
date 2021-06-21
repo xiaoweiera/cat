75fc3f5728c0a3c6cc0f24a27c7c7e2e72fd9c32
yuanxuwei <yuanxuwei@jinse.com>
2021-06-21 21:15:10 +0800

    j

--- src/lib/tool.ts
+++ src/lib/tool.ts
@@ -72,8 +72,11 @@ export function dataToTimestamp(time: string) {
   return dayjs(time).valueOf() / 1000
 }
 // 得到天颗粒度的时间
-export const formatDefaultTime = (date: number) => {
+export const formatDefaultTime = (date: any) => {
   if (date) {
+    if(typeof date !=='number'){
+      return dayjs(date).format('YYYY-MM-DD')
+    }
     return dayjs(parseInt(date+'000')).format('YYYY-MM-DD')
   } else {
     return dayjs().format('YYYY-MM-DD')
