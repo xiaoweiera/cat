fe2654a8b60c6666054d1a2ac80d999545f2e7fa
yuanxuwei <yuanxuwei@jinse.com>
2021-06-28 20:27:59 +0800

    pedding ok

--- src/components/liquidity/sideTop.vue
+++ src/components/liquidity/sideTop.vue
@@ -1,15 +1,23 @@
 <script lang="ts" setup>
+import { ElMessage,ElIcon } from 'element-plus'
 import { ref, reactive, defineProps, onMounted,watch } from 'vue'
-import I18n from '~/utils/i18n'
 import { selectCoin,symbolStore } from '~/store/liquidity/state'
 import { copyToken } from '~/logic/liquidity/dataTool'
 import { toFixedNumber, smallToken } from '~/lib/tool'
-
+import I18n from '~/utils/i18n'
 import { getToken_side } from '~/api/liquidity'
 const props = defineProps({
   symbol: Object,
 })
 const info = ref({})
+const copy=()=>{
+  ElMessage({
+    showClose: true,
+    message: '复制成功',
+    type: 'success'
+  });
+}
+
 watch(()=>symbolStore.id,()=>{
   getInfo()
 })
@@ -41,7 +49,7 @@ onMounted(() => {
           <div class="ml-1.5 text-global-default opacity-85 text-kd14px20px">{{ info.symbol_name }}</div>
           <div class="bg-global-primary ml-1.5 px-1.5 py-0.5 rounded bg-opacity-8 text-global-primary text-kd14px22px">{{ info.exchange }}</div>
         </div>
-        <img src="https://res.ikingdata.com/nav/liquStart.png" class="w-5 h-5" alt=""/>
+<!--        <img src="https://res.ikingdata.com/nav/liquStart.png" class="w-5 h-5" alt=""/>-->
       </div>
       <!-- 涨幅-->
       <div class="flex items-center mt-1.5">
@@ -54,23 +62,12 @@ onMounted(() => {
       <!-- TVL-->
       <div class="flex items-center mt-1.5">
         <span class="text-kd14px20px text-global-default opacity-65">TVL:</span>
-        <span
-          class="text-kd20px28px text-global-default opacity-85 ml-1.5"
-        >${{ info.TVL ? info.TVL : '-' }}</span>
+        <span class="text-kd20px28px text-global-default opacity-85 ml-1.5">${{ info.TVL ? info.TVL : '-' }}</span>
       </div>
       <div class="flex items-center font-kdFang mt-1.5">
-        <span
-          class="text-kd12px16px text-global-default opacity-35"
-        >Token地址</span>
-        <span class="ml-1.5 text-global-primary text-kd14px20px">{{
-            smallToken(info.symbol_id)
-        }}</span>
-        <img
-          class="w-4 h-4 ml-2 hand"
-          src="https://res.ikingdata.com/nav/copy.png"
-          alt=""
-          @click="copyToken(info.symbol_id)"
-        />
+        <span class="text-kd12px16px text-global-default opacity-35">Token地址</span>
+        <span class="ml-1.5 text-global-primary text-kd14px20px">{{smallToken(info.symbol_id) }}</span>
+        <IconFont @click="copy" class="ml-2 hand flex mb-1 opacity-65" type="copy" size="base"></IconFont>
       </div>
     </div>
   </div>
