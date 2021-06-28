324a8bc4c40df0aaaf2e5c7f2c0696c493ee0a9c
yuanxuwei <yuanxuwei@jinse.com>
2021-06-28 12:46:06 +0800

    ikingdata request

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,7 +9,7 @@ interface Config {
 }
 
 export const iconFont = [
-  // 行情&流动性
+  // 行情&流动性呀
   '//at.alicdn.com/t/font_2596648_4rk4tya1d5q.js',
   // 通用
   '//at.alicdn.com/t/font_2596380_g9x84j0fpj.js'
