b3ef457e62a0296d19511d68b867fe4c270d8358
svonme <svon.me@gmail.com>
2021-06-29 17:04:08 +0800

    feat:#KD-345 微调

--- src/components/ui/table/item.vue
+++ src/components/ui/table/item.vue
@@ -12,7 +12,7 @@
     <slot name="label">
       <div class="w-24 text-sm text-kdFang text-global-highTitle leading-10">{{ label }}</div>
     </slot>
-    <div class="md:mt-0" :class="{'mt-2': !!label}">
+    <div class="md:mt-0 flex-auto" :class="{'mt-2': !!label}">
       <slot></slot>
     </div>
   </div>
