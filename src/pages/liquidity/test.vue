bde9c6ce81feb6840c3e37378c3f9e44955c04bc
yuanxuwei <yuanxuwei@jinse.com>
2021-07-06 18:40:51 +0800

    tokenid pairid to href

---
+++ src/pages/liquidity/test.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+// @ts-ignore
+
+</script>
+<template>
+<div   class="w-30 h-30 border-1 animal">aaa</div>
+</template>
+<style>
+.animal{
+  transition: all 2s;
+}
+</style>
+// @formatter:off
+<route lang="yaml">
+meta:
+ layout: liquidityLayout
+</route>
+// @formatter:off
