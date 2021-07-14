b589e86f91247bf5f5a191b17ba96c9ebd62ff24
yuanxuwei <yuanxuwei@jinse.com>
2021-07-14 16:56:50 +0800

    jj

---
+++ src/components/liquidity/full.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps,onMounted,ref,reactive,watch } from 'vue'
+
+</script>
+<template>
+  <FullScreen>
+    <template #default="scope">
+      <el-container v-if="scope.status">
+        <el-header>Header - <FullZoom/></el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+      <div class="flex" v-else>
+        <FullZoom/>
+      </div>
+    </template>
+  </FullScreen>
+</template>
+<style scoped lang="postcss">
+
+</style>
