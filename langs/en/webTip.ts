26d18c1bb016f0b680952b07d2087498004c1425
yuanxuwei <yuanxuwei@jinse.com>
2021-07-28 18:20:50 +0800

    jj

--- langs/en/webTip.ts
+++ langs/en/webTip.ts
@@ -1,16 +1,30 @@
 export default {
     go:'Go',
     newFeatures:'New Function',
-    name: 'DEX Data Analysis 🔥',
-    img: 'https://res.ikingdata.com/liquidity/webTipEn.jpg',
-    title: 'DEX Data Analysis 🔥',
-    href: './liquidity',
-    contents:[
-        'KingData new function online- The first DEX data analysis tool'],
-    descs: [
-        '1. LP+Swap in-depth perspective analysis',
-        '2. Expose runaway project earlier',
-        '3. Find the flow of smart money',
-        '4. Investor behavior is clear at a glance'
-    ]
+    fixIcon:'🎉',
+    fix:'更新优化',
+    liquidity:{
+        name: 'DEX Data Analysis 🔥',
+        img: 'https://res.ikingdata.com/liquidity/webTipEn.jpg',
+        title: 'DEX Data Analysis 🔥',
+        href: './liquidity',
+        contents:[
+            'KingData new function online- The first DEX data analysis tool'],
+        descs: [
+            '1. LP+Swap in-depth perspective analysis',
+            '2. Expose runaway project earlier',
+            '3. Find the flow of smart money',
+            '4. Investor behavior is clear at a glance'
+        ]
+    },
+    fix1:{
+        name: '7月28日更新',
+        href: './liquidity',
+        descs: [
+            '1. 优化了数据图表的体验',
+            '2. 修复了数据图表中已知的数据（SUSHI数据修复、MDEX项目数据修复、MDX Token 数据修复、UniSwap数据修复、DeFi数据修复、AMPL数据修复',
+            '3. 新增了UNISWAP V3流动性数据',
+            '4. 修复了APY大全目前已知的问题'
+        ]
+    },
 }
