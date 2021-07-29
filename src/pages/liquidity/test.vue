ab6fcfc5052b2be1c4dcfa368bd4393336027719
yuanxuwei <yuanxuwei@jinse.com>
2021-07-29 15:34:19 +0800

    j

--- src/pages/liquidity/test.vue
+++ src/pages/liquidity/test.vue
@@ -1,85 +1,72 @@
 <script lang="ts" setup>
-// @ts-ignore
-class person{
-  name:string;
-  constructor(name:string){
-    this.name=name
-  }
-  private desc:string='desc'
-  static getName():void{
-    alert('aa')
-  }
+ const headers = [
+  { label: '地址', key: 'address' },
+  { label: '净添加量', key: 'netAddNumber' },
+  { label: '添加总量', key: 'allNumber' },
+  { label: '移除总量', key: 'removeNumber' },
+  { label: '参与次数', key: 'joinNumber' },
+   { label: '添加次数', key: 'addNumber' },
+   { label: '移除次数', key: 'removeCount' }
+]
+ const data=[
+   {
+     address:'北京',
+     netAddNumber:'1000',
+     allNumber:'2000',
+     removeNumber:'1200',
+     joinNumber:'1100',
+     addNumber:'888',
+     removeCount:'123'
+   },
+   {
+     address:'石家庄',
+     netAddNumber:'1300',
+     allNumber:'4000',
+     removeNumber:'3200',
+     joinNumber:'2100',
+     addNumber:'688',
+     removeCount:'1231'
+   }
+ ]
+const headerBg = () => {
+  return 'background: rgba(43, 141, 254, 0.04)'
 }
-class person2 extends person{
-  name:string;
-
-}
-const j=''
-const p=new person2('小明')
-alert(p.desc)
 </script>
 <template>
-  <el-popover
-      placement="bottom"
-      title="标题"
-      :width="200"
-      trigger="hover"
-  >
-    <div>aaa</div>
-    <template #reference>
-      <el-button>hover 激活</el-button>
+<div class="w-300">
+  <el-table class="mt-20" :data="data" stripe >
+    <template v-for="(item, index) in headers" :key="index">
+      <!-- 节点名称 -->
+      <el-table-column
+          v-if="item.key === 'node_name'"
+          :prop="item.key"
+          :label="item.label"
+      >
+        <template #default="scope">
+          <div class="flex items-center">
+
+            <p class="whitespace-nowrap">2</p>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column
+          v-else
+          :prop="item.key"
+          :label="item.label"
+      >
+        <template #default="scope">
+          <span class="text-sm whitespace-nowrap"  :class="item.className">{{ scope.row[item.key] }}</span>
+        </template>
+      </el-table-column>
     </template>
-  </el-popover>
+  </el-table>
+</div>
+
 
 </template>
 <style>
-.auto            { cursor: auto; }
-.deafult         { cursor: default; }
-.none            { cursor: none; }
-.context-menu    { cursor: context-menu; }
-.help            { cursor: help; }
-.pointer         { cursor: pointer; }
-.progress        { cursor: progress; }
-.wait            { cursor: wait; }
-.cell            { cursor: cell; }
-.crosshair       { cursor: crosshair; }
-.text            { cursor: text; }
-.vertical-text   { cursor: vertical-text; }
-.alias           { cursor: alias; }
-.copy            { cursor: copy; }
-.move            { cursor: move; }
-.no-drop         { cursor: no-drop; }
-.not-allowed     { cursor: not-allowed; }
-.all-scroll      { cursor: all-scroll; }
-.col-resize      { cursor: col-resize; }
-.row-resize      { cursor: row-resize; }
-.n-resize        { cursor: n-resize; }
-.e-resize        { cursor: e-resize; }
-.s-resize        { cursor: s-resize; }
-.w-resize        { cursor: w-resize; }
-.ns-resize       { cursor: ns-resize; }
-.ew-resize       { cursor: ew-resize; }
-.ne-resize       { cursor: ne-resize; }
-.nw-resize       { cursor: nw-resize; }
-.se-resize       { cursor: se-resize; }
-.sw-resize       { cursor: sw-resize; }
-.nesw-resize     { cursor: nesw-resize; }
-.nwse-resize     { cursor: nwse-resize; }
-.animal{
-  //height: calc( 100vh - 75px);
-  //position: absolute;
-  background: red;
-  //top:75px;
-  //bottom:0;
-  transition: all 1s;
-  //transform: translate(10px, 0);
-  //margin-left:50%;
-}
-.animal:hover{
-  font-size: 100px;
-  background: white;
-  @apply w-50 h-50;
-}
+
+
 </style>
 // @formatter:off
 <route lang="yaml">
