85e45db8d95c0a3758747efd4d5ba16662a86ff6
yuanxuwei <yuanxuwei@jinse.com>
2021-08-04 19:01:43 +0800

    jj

--- src/components/liquidity/fullChart/payDown.vue
+++ src/components/liquidity/fullChart/payDown.vue
@@ -132,9 +132,9 @@ const scrollFun=()=>{
           </div>
         </template>
         <div class="w-full mb-1 text-center text-kd12px18px text-global-time">
-          <div v-if="hasData && loading">加载中...</div>
-          <div v-if="hasData && !loading">上拉加载更多</div>
-          <div v-if="!hasData">没有更多了</div>
+<!--          <div v-if="hasData && loading">加载中...</div>-->
+<!--          <div v-if="hasData && !loading">上拉加载更多</div>-->
+<!--          <div v-if="!hasData">没有更多了</div>-->
         </div>
       </div>
     </div>
