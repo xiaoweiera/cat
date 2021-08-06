a65c3eef9599a94af52b10cca94b0183cd967026
yuanxuwei <yuanxuwei@jinse.com>
2021-08-06 19:01:34 +0800

    jj

---
+++ src/components/apyTwo/chains.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, defineProps } from 'vue'
+import {chains} from '~/logic/apyTwo/mining/config'
+import * as R from 'ramda'
+import I18n from '~/utils/i18n/index'
+const props = defineProps({
+  filterData: Object,
+  tagType: String,
+})
+
+</script>
+<template>
+<div>
+  <template v-for="item in chains">
+    <img class="w-4 h-4" :src="item.icon" alt="">
+    <div></div>
+  </template>
+</div>
+</template>
+<style scoped lang="postcss">
+
+</style>
---
+++ src/pages/apyTwo/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+// @ts-ignore
+import {ref} from 'vue'
+</script>
+<template>
+<div>
+  <ApyTwoChains />
+</div>
+</template>
+<style scoped>
+
+</style>
+// @formatter:off
+<route lang="yaml">
+meta:
+ layout: liquidityLayout
+</route>
+// @formatter:off
