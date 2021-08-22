71c248e21bb4069de78b72d5a09a107e1128b695
svonme <svon.me@gmail.com>
2021-08-23 01:34:33 +0800

    feat:#kd-811 币种详情

--- src/components/apy2/token/index.vue
+++ src/components/apy2/token/index.vue
@@ -3,74 +3,70 @@
  * @file 币种分析
  * @author svon.me@gmail.com
  */
-import { onMounted, toRaw, computed } from 'vue'
+import { onBeforeMount, computed, toRaw } from 'vue'
 import { tokenList } from '~/store/apy2/state'
 // @ts-ignore
-import makeRouterPath from '~/utils/router'
+import { getParam } from '~/utils/router'
 // @ts-ignore
-import { ready } from '~/logic/apy2/token'
+import { ready, TabCategoryData } from '~/logic/apy2/token'
 import { useProvide } from '~/utils/use/state'
-import { useRoute } from 'vue-router'
-import safeGet from '@fengqiaogang/safe-get'
-import safeSet from '@fengqiaogang/safe-set'
-import { uuid, equalsIgnoreCase } from '~/utils'
+import { uuid, equalsIgnoreCase, toInteger } from '~/utils'
 import DBList from '@fengqiaogang/dblist'
 
 // @ts-ignore
 const [ date ] = useProvide('uiDate')
 
-const TabData = {
-  mining: 'mining',
-  deposit: 'deposit'
+
+// 获取
+const getActiveTokenId = function(): string | number {
+  const value = getParam<string | number>('id')
+  if (value) {
+    return value as string
+  }
+  const list = tokenList.value
+  const [ first ] = list
+  return first?.id
 }
 
-const tabList = [
-  {
-    id: TabData.mining,
-    name: '挖矿收益'
-  }, {
-    id: TabData.deposit,
-    name: '利率收益'
+const getTabList = function() {
+  const id = getActiveTokenId()
+  const db = new DBList(toRaw(tokenList.value), 'id')
+  const symbol = db.selectOne<any>({ id: toInteger(id) })
+  if (symbol && symbol.category && symbol.category.length > 0) {
+    const tab = new DBList([
+      {
+        id: TabCategoryData.mining,
+        name: '挖矿收益',
+      }, {
+        id: TabCategoryData.deposit,
+        name: '利率收益'
+      }
+    ], 'id')
+    return tab.select({ id: symbol.category })
   }
-]
-
-// 获取当前路由对象
-const router = useRoute()
-
-const getActiveToken = function() {
-  const $router = toRaw(router)
-  const query = $router.query.value as any
-  const list = toRaw(tokenList.value)
-  if (query && query.id) {
-    const db = new DBList(list, 'id')
-    const where = { id: query.id }
-    const item = db.selectOne(where)
-    if (item) {
-      return item.id
-    }
-  }
-  return safeGet(list, '[0].id') || uuid()
+  return []
 }
 
-const currentTokenId = computed(getActiveToken)
+// @ts-ignore
+const tabList = computed(getTabList)
 
-const isRouterActive = function(item: any, type: string) {
-  const $router = toRaw(router)
-  const query = $router.query.value as any
-  // 处理默认值
-  if (!safeGet(query, type)) {
-    if (type === 'type') {
-      safeSet(query, type, safeGet(tabList, '[0].id'))
-    }
-    if (type === 'id') {
-      safeSet(query, type, getActiveToken())
-    }
+// @ts-ignore
+const contentKey = computed(function() {
+  const id = getActiveTokenId()
+  const query = { ...getParam(), id }
+  return uuid(JSON.stringify(query))
+})
+
+// @ts-ignore
+const isRouterActive = function(id: string | number) {
+  const value = getActiveTokenId()
+  if (value) {
+    return equalsIgnoreCase(value, id);
   }
-  return equalsIgnoreCase(query[type], item[type]);
+  return false
 }
 
-onMounted(ready)
-
+onBeforeMount(ready)
 
 </script>
 
@@ -91,10 +87,10 @@ onMounted(ready)
           <div class="pt-3 pb-10" v-if="tokenList.length > 0">
             <template v-for="(item, index) in tokenList" :key="index">
               <div class="cursor-pointer">
-                <router-link class="flex items-center p-1.5" :to="item.href" :class="{'menu-active': isRouterActive(item, 'id')}">
+                <router-link class="flex items-center p-1.5" :to="item.href" :class="{'menu-active': isRouterActive(item.id)}">
                   <span class="inline-flex">
-                    <!-- rounded-xl overflow-hidden -->
-                    <IconFont rounded :type="item.icon" size="24"></IconFont>
+                    <IconFont rounded v-if="item.icon" :type="item.icon" size="24"></IconFont>
+                    <IconFont v-else type="icon-morentoken" size="40"/>
                   </span>
                   <span class="ml-1.5">
                     <span class="text-sm text-global-highTitle">{{ item.name }}</span>
@@ -108,37 +104,8 @@ onMounted(ready)
     </template>
 
     <template #content>
-      <div class="p-8 text-kdFang">
-        <div class="flex justify-between">
-          <div class="flex items-center">
-            <IconFont type="icon-facebook-circle-fill" size="40" rounded/>
-            <b class="title ml-2 font-bold text-global-highTitle text-opacity-85">BTC</b>
-            <span class="ml-4 text-xl text-global-highTitle text-opacity-85">$21312.2</span>
-            <span class="ml-2 bg-global-numRed inline-block py-0.5 px-1 rounded">
-              <span class="text-base text-global-white">-3.23%</span>
-            </span>
-          </div>
-          <div class="flex items-center rounded-xl bg-global-highTitle bg-opacity-6 p-1">
-            <template v-for="item in tabList" :key="item.id">
-              <router-link class="page-switch" :class="{'active': isRouterActive({ type: item.id }, 'type')}" :to="makeRouterPath({query: { type: item.id }})">
-                <IconFont type="icon-danbi" size="24"/>
-                <span class="ml-2">{{ item.name }}</span>
-                <IconFont class="ml-1.5" type="icon-star-xuanzhong" size="16"/>
-              </router-link>
-            </template>
-          </div>
-        </div>
-
-        <div class="mt-8 pt-0.5" :key="currentTokenId">
-          <!--挖矿收益-->
-          <div v-if="isRouterActive({ type: TabData.mining }, 'type')">
-            <Apy2TokenMining/>
-          </div>
-          <!--利率收益-->
-          <div v-else-if="isRouterActive({ type: TabData.deposit }, 'type')">
-            <Apy2TokenDeposit/>
-          </div>
-        </div>
+      <div class="p-8" v-if="getActiveTokenId()">
+        <Apy2TokenContent :key="contentKey" :id="getActiveTokenId()" :category="tabList"/>
       </div>
     </template>
   </UiLayoutMenu>
@@ -155,16 +122,6 @@ onMounted(ready)
 }
 .menu-active {
   @apply bg-global-highTitle bg-opacity-6 rounded;
-}
-.title {
-  font-size: 1.75rem;
-}
-.page-switch {
-  @apply inline-block h-9 flex items-center px-3.5 rounded-lg overflow-hidden text-global-highTitle text-opacity-65;
-  &.active {
-    @apply bg-global-white text-global-primary text-opacity-100;
-    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.06), 0 1px 2px rgba(0, 0, 0, 0.1);
-  }
 }
 
 .el-button--small {
--- src/utils/router.ts
+++ src/utils/router.ts
@@ -3,7 +3,31 @@
  * @description 携带中英文标识
  * @author svon.me@gmail.com
  */
+
 import { router } from '~/utils/directive/router'
+import safeGet from '@fengqiaogang/safe-get'
+import { useRoute } from 'vue-router'
+import { toRaw } from 'vue'
+
+// 获取 url 对象
+export const getLocation = function() {
+  const location = useRoute()
+  const $router = toRaw(location)
+  return {
+    // @ts-ignore
+    path: $router.path.value,
+    // @ts-ignore
+    query: $router.query.value,
+  }
+}
+// 获取参数
+export const getParam = function<T>(key?: string, defaultValue?: T) {
+  const location = getLocation()
+  if (key) {
+    return safeGet<T>(location, `query.${key}`) || defaultValue
+  }
+  return safeGet<Object>(location, 'query')
+}
 
 export const config = {
   topic: '/topic',
