8b3ee045594c955a9a55f62b944fe814e2cf5e55
yuanxuwei <yuanxuwei@jinse.com>
2021-08-24 11:12:20 +0800

    chongtu

--- src/components/apy2/mining.vue
+++ src/components/apy2/mining.vue
@@ -19,7 +19,6 @@ onMounted(async ()=>{
 })
 </script>
 <template>
-  {{rankingTag}}--
   <div class=" flex justify-center    bg-global-white  h-full font-kdFang " >
     <div class="max-w-360  px-20  relative  w-full h-full container ">
       <div class="flex items-center justify-between">
