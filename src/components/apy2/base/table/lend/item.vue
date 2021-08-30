206f751cab65956e65f8d7259b7508bee63c6154
svonme <svon.me@gmail.com>
2021-08-30 16:30:18 +0800

    feat:#kd-811 借贷表格数据展示

--- src/components/apy2/base/table/lend/item.vue
+++ src/components/apy2/base/table/lend/item.vue
@@ -18,8 +18,8 @@ defineProps({
       <div class="apy-item">
       <el-popover popper-class="apy-hover-detail" placement="bottom-start" title="" width="fit-content" :show-after="300" trigger="hover" :show-arrow="false" :offset="0">
         <template #reference>
-          <div class="h-18.5 flex items-center">
-            <div class="pl-4 pr-2 text-kdFang">
+          <div class="h-18.5 flex items-center w-full">
+            <div class="pl-4 pr-2 text-kdFang w-full">
               <div>
                 <span class="text-global-numGreen font-bold text-xl">{{ toNumber(data.apy) }}%</span>
               </div>
@@ -28,12 +28,17 @@ defineProps({
                 <span class="text-global-highTitle text-opacity-45 ml-1">可借金额</span>
                 <span class="text-global-highTitle text-opacity-85 ml-1">${{ numberUint(data.quota_remain) }} ({{ toNumber(data.quota_remain_percent) }}%)</span>
               </div>
-              <div class="text-xs mt-1 flex items-center">
-                <IconFont :type="data.chain" size="14"/>
-                <IconFont :type="data.project_category" size="14" class="ml-1"/>
-                <span class="inline-block bg-global-highTitle bg-opacity-6 py-0.5 px-1 rounded ml-1" v-if="data.strategy_tags">
-                <span class="text-global-highTitle text-opacity-45 ml-1">{{ data.strategy_tags }}</span>
-              </span>
+              <div class="text-xs mt-1 flex justify-between">
+                <div class="flex items-center">
+                  <IconFont :type="data.chain" size="14"/>
+                  <IconFont :type="data.project_category" size="14" class="ml-1"/>
+                  <span class="inline-block bg-global-highTitle bg-opacity-6 py-0.5 px-1 rounded ml-1" v-if="data.strategy_tags">
+                    <span class="text-global-highTitle text-opacity-45 ml-1">{{ data.strategy_tags }}</span>
+                  </span>
+                </div>
+                <div class="text-global-highTitle text-opacity-25">
+                  <IconFont type="icon-info2" size="16"/>
+                </div>
               </div>
             </div>
           </div>
--- src/components/apy2/base/table/mining/item.vue
+++ src/components/apy2/base/table/mining/item.vue
@@ -18,8 +18,8 @@ defineProps({
       <div class="apy-item">
         <el-popover popper-class="apy-hover-detail" placement="bottom-start" title="" width="fit-content" :show-after="300" trigger="hover" :show-arrow="false" :offset="0">
           <template #reference>
-            <div class="h-18.5 flex items-center">
-              <div class="pl-4 pr-2 text-kdFang">
+            <div class="h-18.5 flex items-center w-full">
+              <div class="pl-4 pr-2 text-kdFang w-full">
                 <div>
                   <span class="text-global-numGreen font-bold text-xl">{{ toNumber(data.apy) }}%</span>
                   <span class="ml-1 text-xs text-global-highTitle text-opacity-45" v-if="toInteger(data.lever) > 0">{{ toInteger(data.lever) }}X</span>
@@ -29,12 +29,17 @@ defineProps({
                   <span class="text-global-highTitle text-opacity-45 ml-1">TVL</span>
                   <span class="text-global-highTitle text-opacity-85 ml-1">${{ numberUint(data.tvl) }}</span>
                 </div>
-                <div class="text-xs mt-1 flex items-center">
-                  <IconFont :type="data.chain" size="14"/>
-                  <IconFont :type="data.project_category" size="14" class="ml-1"/>
-                  <span class="inline-block bg-global-highTitle bg-opacity-6 py-0.5 px-1 rounded ml-1" v-if="data.strategy_tags">
-              <span class="text-global-highTitle text-opacity-45 ml-1">{{ data.strategy_tags }}</span>
-            </span>
+                <div class="text-xs mt-1 flex justify-between">
+                  <div class="flex items-center">
+                    <IconFont :type="data.chain" size="14"/>
+                    <IconFont :type="data.project_category" size="14" class="ml-1"/>
+                    <span class="inline-block bg-global-highTitle bg-opacity-6 py-0.5 px-1 rounded ml-1" v-if="data.strategy_tags">
+                    <span class="text-global-highTitle text-opacity-45 ml-1">{{ data.strategy_tags }}</span>
+                  </span>
+                  </div>
+                  <div class="text-global-highTitle text-opacity-25">
+                    <IconFont type="icon-info2" size="16"/>
+                  </div>
                 </div>
               </div>
             </div>
