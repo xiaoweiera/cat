272dc6483b9995fc1ea28809a2d9fa2fa17119dd
svonme <svon.me@gmail.com>
2021-09-02 21:16:08 +0800

    feat:导航部分中英文

---
+++ langs/cn/menu.ts
@@ -0,0 +1,18 @@
+export default {
+  apy: {
+    mining: '挖矿 APY',
+    loan: '借贷APR',
+    project: '项目 APY 分析',
+    token: '币种 APY 分析',
+    heco: 'HECO 节点 APY',
+  },
+  desc: {
+    apy: {
+      mining: '快速发现高收益矿池',
+      loan: '找到抵押借贷最佳路径',
+      project: '项目自身、池子数据',
+      token: '币种相关存借信息',
+      heco: 'HECO 节点竞选 APY',
+    },
+  }
+}
