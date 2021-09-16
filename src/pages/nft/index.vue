14fc17040522304897cb6d30168b752ef7b32dae
svonme <svon.me@gmail.com>
2021-09-16 18:29:05 +0800

    feat:默认链接

---
+++ src/pages/nft/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+import { config, getParam } from '~/utils/router'
+
+onBeforeMount(() => {
+  const router = useRouter()
+  const query: any = getParam<object>()
+  router.replace({
+    path: `${config.nft}/discover`,
+    query: query
+  })
+})
+</script>
+
+<template>
+  <div class="min-h-150">
+    <Spin :loading="true"></Spin>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: all
+</route>
