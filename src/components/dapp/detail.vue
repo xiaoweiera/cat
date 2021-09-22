73ab5ecd914cbdc65d890943a06dad155bc7ceca
svonme <svon.me@gmail.com>
2021-09-22 17:41:43 +0800

    feat:dapp 详情

---
+++ src/components/dapp/detail.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="md:w-250 mx-auto p-6 bg-white rounded-sm">
+    <div class="flex">
+      <el-avatar shape="square" fit="fit" :size="92" src="http://iph.href.lu/92x92"></el-avatar>
+      <div class="mx-4 flex-1 w-1">
+        <p>
+          <span class="text-18">N.Fans</span>
+          <span class="ml-1.5">nft</span>
+          <span class="ml-1.5">机枪池</span>
+        </p>
+        <p class="text-14-20 mt-2.5">
+          N.Fans is committed to creating a metaverse of animations and GameFi with and for globally recognized IPs.N.Fans is committed to creating a metaverse of animations and GameFi with and for globally recognized IPs.N.Fans is committed to creating a metaverse of animations and GameFi with and for globally recognized...
+        </p>
+      </div>
+      <div>
+        <DappStar/>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
