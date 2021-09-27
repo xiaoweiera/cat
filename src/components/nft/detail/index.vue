3c3448ade647a9378735006f7569ffd4eee06650
svonme <svon.me@gmail.com>
2021-09-27 16:42:42 +0800

    feat:#1221 dapp 详情微调

--- src/components/nft/detail/index.vue
+++ src/components/nft/detail/index.vue
@@ -22,33 +22,32 @@ onMounted(getDetail)
 </script>
 
 <template>
-  <div v-if="loading" class="md:w-250 mx-auto p-6 bg-white rounded-sm">
+  <div v-if="loading" class="md:w-250 mx-auto p-4 md:p-6 bg-white rounded-sm">
     <div class="min-h-100">
       <Spin :loading="true"></Spin>
     </div>
   </div>
-  <div v-else class="md:w-250 mx-auto p-6 bg-white rounded-sm">
+  <div v-else class="md:w-250 mx-auto p-4 md:p-6 bg-white rounded-sm">
     <div class="flex md:hidden pb-4">
       <template v-for="(value, index) in data.gallery" :key="index">
         <el-avatar v-if="value" class="mr-2" shape="square" fit="fit" :size="64" :src="value"></el-avatar>
       </template>
     </div>
-    <div class="flex items-center">
-      <DappDetailTitle class="nft-title" :data="data"/>
-    </div>
-    <div class="hidden md:flex items-end">
-      <div class="mr-4 flex-1">
-        <div class="mt-2">
-          <template v-for="(value, index) in data.gallery" :key="index">
-            <el-image v-if="value" class="mr-2 w-23 h-23" shape="square" fit="fit" :preview-src-list="[value]" :src="value"/>
-          </template>
+    <div class="md:flex">
+      <div class="md:mr-4 md:flex-1">
+        <div class="flex items-center">
+          <DappDetailTitle class="nft-title" :data="data"/>
         </div>
         <div class="mt-2">
           <p class="text-14-20 mt-2.5 text-global-highTitle text-opacity-85">{{ data.description }}</p>
         </div>
+        <div class="mt-2 hidden md:flex">
+          <template v-for="(value, index) in data.gallery" :key="index">
+            <el-image v-if="value" class="mr-2 w-23 h-23" shape="square" fit="fit" :preview-src-list="[value]" :src="value"/>
+          </template>
+        </div>
       </div>
-      <!-- 点赞 -->
-      <div>
+      <div class="hidden md:block">
         <DappStar :id="data.id" :value="data.clout" :clouted="data.is_clouted"/>
       </div>
     </div>
